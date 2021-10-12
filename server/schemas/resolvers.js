08afa00dec21390cc3326771d2fe377cae4f1f53
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-12 12:34:11 -0500

    update resolver mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,32 +1,52 @@
-const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-// const { signToken } = require('../utils/auth');
+const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-      me:async (parent, args, context) => {
-        if (context.user) {
-          const userInfo = await User.findOne({_id: context.user._id})
-          .select('-__v -password')
-          .populate('savedBooks')
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userInfo = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("savedBooks");
 
-          return userInfo 
-        }
-        
-        throw new AuthenticationError('Not logged in')
+        return userInfo;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+    books: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Book.find(params).sort({ createdAt: -1 });
+    },
+    book: async (parent, { _id }) => {
+      return Book.findOne({ _id });
     },
     mutation: {
       addUser: async (parent, args) => {
         const user = await User.create(args);
-  
+        const token = signToken(user);
+
         return user;
       },
-      login: async () => {
-  
-      }
-    }
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
 
-  }
-}
-  
-  module.exports = resolvers;
+        if (!user) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const token = signToken(user);
+        return { token, user };
+      },
+    },
+  },
+};
+
+module.exports = resolvers;
